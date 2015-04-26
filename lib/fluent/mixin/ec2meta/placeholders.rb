ea27a98304874124a91d45181a8c7dad4e11ba59
hirakiuc <hirakiuc@gmail.com>
2015-04-26 23:31:02 +0900

    initial import

---
+++ lib/fluent/mixin/ec2meta/placeholders.rb
@@ -0,0 +1,11 @@
+require "fluent/mixin/ec2meta/placeholders/version"
+
+module Fluent
+  module Mixin
+    module Ec2meta
+      module Placeholders
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/fluent/mixin/ec2meta/placeholders/version.rb
@@ -0,0 +1,9 @@
+module Fluent
+  module Mixin
+    module Ec2meta
+      module Placeholders
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
